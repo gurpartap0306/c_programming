51857aa61d236e8cca6dce29175b70a2d0561e6d
gurpartap0306 <dhaliwal.gurpartap@gmail.com>
2020-08-16 11:01:27 +0530

    fun

--- examples/hello.c
+++ examples/hello.c
@@ -1,6 +1,7 @@
-#include</home/user/Desktop/c_programming/helo.h>
-
+//#include</home/user/Desktop/c_programming/helo.h>
+#include"helo.h"
+#include<stdio.h>
 void main(){
 	print();
-	
+	printf("hello,world\c");	
 }
